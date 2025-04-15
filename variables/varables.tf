ed07491141f3d527b86e0b2bb1b41c045eb75320
ram-pavani <ulasala0@gmail.com>
2025-04-15 08:00:46 +0530

    terrafom variables

---
+++ variables/varables.tf
@@ -0,0 +1,42 @@
+variable "ami_id" {
+  type = string
+  default = "ami-09c813fb71547fc4f"
+  description = "this is RHEL9 AMI ID"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "ec2_tags" {
+  type = map
+  default = {
+    project = "expense"
+    component = "backend"
+    environment = "dev"
+    name = "expense-backend-dev"
+  }
+}
+
+variable "from_port" {
+  type = number
+  default = 22
+}
+
+variable "to_port" {
+  type = number
+  default = 22
+}
+
+variable "cidr_blocks" {
+  type = list(string)
+  default = [ "0.0.0.0/0" ]
+}
+
+variable "sg_tags" {
+  type = map
+  default = {
+    name = "expense-backend-dev"
+  }
+}
+
