82742b5a9cef5622c0652b029d4bf96825aaf563
ram-pavani <ulasala0@gmail.com>
2025-04-16 10:58:44 +0530

    terrafom locals

---
+++ locals/ec2.tf
@@ -0,0 +1,31 @@
+resource "aws_instance" "this" {
+  ami = local.ami_id
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  instance_type = "t3.micro"
+  tags = {
+    name ="terraform-demo"
+    purpose = "terraform-practice"
+  }
+}
+resource "aws_security_group" "allow_tls" {
+  name = "allow_tls"
+  description = "allow TLS inbound traffic and all outbound traffic"
+
+  tags = {
+    name = "allow_tls"
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
