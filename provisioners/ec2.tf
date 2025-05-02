ac22997dae89c1200d5033f8895f79a3f301ab65
ram-pavani <ulasala0@gmail.com>
2025-05-02 15:02:32 +0530

    provisioners

---
+++ provisioners/ec2.tf
@@ -0,0 +1,35 @@
+resource "aws_instance" "this" {
+  ami                    = "ami-09c813fb71547fc4f"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  instance_type          = "t3.micro"
+  tags = {
+    name    = "terraform-demo"
+    purpose = "terraform-practice"
+  }
+  provisioner "local-exec" {
+    command = "echo ${self.private_ip} > inventory"
+  }
+
+}
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "allow TLS inbound traffic and all outbound traffic"
+
+  tags = {
+    name = "allow_tls"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
