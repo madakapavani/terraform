cbd4e8a1c4c98b8d18b6a59101b84b9e4777591b
ram-pavani <ulasala0@gmail.com>
2025-05-02 15:13:01 +0530

    provisioners

--- provisioners/ec2.tf
+++ provisioners/ec2.tf
@@ -1,24 +1,47 @@
 resource "aws_instance" "this" {
-  ami                    = "ami-09c813fb71547fc4f"
+  ami                    = "ami-09c813fb71547fc4f" # This is our devops-practice AMI ID
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   instance_type          = "t3.micro"
   tags = {
-    name    = "terraform-demo"
-    purpose = "terraform-practice"
+    Name    = "terraform-demo"
+    Purpose = "terraform-practice"
   }
+
   provisioner "local-exec" {
     command = "echo ${self.private_ip} > inventory"
   }
 
-}
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "allow TLS inbound traffic and all outbound traffic"
-
-  tags = {
-    name = "allow_tls"
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = self.public_ip
   }
 
+  provisioner "remote-exec" {
+    inline = [
+      "sudo dnf install nginx -y",
+      "sudo systemctl start nginx",
+    ]
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
@@ -26,10 +49,7 @@ resource "aws_security_group" "allow_tls" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  tags = {
+    Name = "allow_tls"
   }
 }
